9581b6748982fc8244641f9c2edb3b18a04e1d95
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-09-22 22:05:47 +0200

    happy clietns

--- src/components/happyClient.js
+++ src/components/happyClient.js
@@ -1,27 +1,23 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
 
+import "./happyClient.css"
 
+const HappyClient = ({}) => {
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => `<b>${text}</b>`,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, next) => `<p>${next(node.content)}</p>`,
+    },
+  }
 
-  import React from "react"
-  import { useStaticQuery, graphql } from "gatsby"
-  import { BLOCKS, MARKS } from "@contentful/rich-text-types"
-  import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
-  
-  import "./whatWeDo.css"
- 
- const HappyClient = ({   }) => {
-
-    const options = {
-        renderMark: {
-          [MARKS.BOLD]: text => `<b>${text}</b>`,
-        },
-        renderNode: {
-          [BLOCKS.PARAGRAPH]: (node, next) => `<p>${next(node.content)}</p>`,
-        },
-      }
-    
-   const data = useStaticQuery(graphql`
-   query HappyClientQuery {
-    contentfulNewPage {
+  const data = useStaticQuery(graphql`
+    query HappyClientQuery {
+      contentfulNewPage {
         contentModules {
           ... on ContentfulLayoutClientSection {
             id
@@ -38,33 +34,30 @@
           }
         }
       }
-  }
-   `)
- 
-   const happyClientData = data.contentfulNewPage.contentModules.find(x => x.contentful_id === "3m6u3iOaio88ppMoetJP3I")
-  
+    }
+  `)
+
+  const happyClientData = data.contentfulNewPage.contentModules.find(
+    x => x.contentful_id === "3m6u3iOaio88ppMoetJP3I"
+  )
+
   console.log(happyClientData)
-   return (
-     
-        <div
-        className="highlightWrap"
-       > 
-     
-     
-    <h4>Just a few of our happy clients</h4>
-    {happyClientData.layoutClientImage.map((c,i) => {
+  return (
+    <div className="happyClientWrap">
+      <h4>Just a few of our happy clients</h4>
+      <div className="clientWrap">
+        {happyClientData.layoutClientImage.map((c, i) => {
+          return <img key={i + "image"} src={c.logo.fluid.src} alt="" />
+        })}
+        {happyClientData.layoutClientImage.map((c, i) => {
+          return <img key={i + "image"} src={c.logo.fluid.src} alt="" />
+        })}
+      </div>
+      <a className="discover" href="">
+        Discover how we make them happy
+      </a>
+    </div>
+  )
+}
 
-        return (
-
-            <img key={i + "image"} src={c.logo.fluid.src} alt=""/>
-        )
-
-    })}
-       </div>
-     
-   )
- }
-  
- 
- export default HappyClient
- 
+export default HappyClient
