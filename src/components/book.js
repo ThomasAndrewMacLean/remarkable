6b0b9a4655fbabcc46126555932feaef7c273c73
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-09-22 21:56:59 +0200

    book styling

--- src/components/book.js
+++ src/components/book.js
@@ -1,40 +1,36 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
 
+import "./book.css"
 
+const Book = ({}) => {
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => `<b>${text}</b>`,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, next) => `<p>${next(node.content)}</p>`,
+    },
+  }
 
-  import React from "react"
-  import { useStaticQuery, graphql } from "gatsby"
-  import { BLOCKS, MARKS } from "@contentful/rich-text-types"
-  import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
-  
-  import "./whatWeDo.css"
- 
- const Book = ({   }) => {
-
-    const options = {
-        renderMark: {
-          [MARKS.BOLD]: text => `<b>${text}</b>`,
-        },
-        renderNode: {
-          [BLOCKS.PARAGRAPH]: (node, next) => `<p>${next(node.content)}</p>`,
-        },
-      }
-    
-   const data = useStaticQuery(graphql`
-   query BookQuery {
-    contentfulNewPage {
+  const data = useStaticQuery(graphql`
+    query BookQuery {
+      contentfulNewPage {
         contentModules {
-            ... on ContentfulLayoutHighlightedImage {
-                contentful_id
-                cta {
-                    cta
-                    json
-                  }
-                image {
-                  fluid {
-                    src
-                  }
-                }
+          ... on ContentfulLayoutHighlightedImage {
+            contentful_id
+            cta {
+              cta
+              json
+            }
+            image {
+              fluid {
+                src
               }
+            }
+          }
           ... on ContentfulLayoutContentBlock {
             id
 
@@ -42,38 +38,47 @@
             title
             headline
             ctaTitle
-            copy {
-              copy
+            copyRich {
+              json
             }
           }
         }
       }
-  }
-   `)
- 
-   const bookData = data.contentfulNewPage.contentModules.find(x => x.contentful_id === "71JJMBCHq3v9O6TFSPO9Aa")
-   const imageData = data.contentfulNewPage.contentModules.find(x => x.contentful_id === "4pf5Wo8fXGzpcCaYbQGbTM")
-    return (
-     
-        <div
-        className="highlightWrap"
-       > 
-     
-     
-    <h4>{bookData.headline}</h4>
-     <p
-                
-              > {bookData.copy.copy}</p>
+    }
+  `)
 
-{bookData.ctaTitle}
-<img src={imageData.image.fluid.src} alt=""/>
+  const bookData = data.contentfulNewPage.contentModules.find(
+    x => x.contentful_id === "71JJMBCHq3v9O6TFSPO9Aa"
+  )
+  const imageData = data.contentfulNewPage.contentModules.find(
+    x => x.contentful_id === "4pf5Wo8fXGzpcCaYbQGbTM"
+  )
+  return (
+    <div className="bookWrap">
+      <div className="textWrap">
+        <h4>{bookData.headline}</h4>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: documentToHtmlString(bookData.copyRich.json, options),
+          }}
+        ></p>
 
-<button dangerouslySetInnerHTML={{__html:documentToHtmlString(imageData.cta.json, options)}}></button>
-       </div>
-     
-   )
- }
-  
- 
- export default Book
- 
+        <a href="">{bookData.ctaTitle}</a>
+      </div>
+
+      <div className="imageWrap">
+        <img src={imageData.image.fluid.src} alt="" />
+        <div className="center">
+          <button
+            className="secondaryButton"
+            dangerouslySetInnerHTML={{
+              __html: documentToHtmlString(imageData.cta.json, options),
+            }}
+          ></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Book
